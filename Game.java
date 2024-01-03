2666feac18d34c371687b638f7b346304db88d7d
Asuna's Lover <kiritoadm@mail.ru>
2024-01-03 20:19:09 +0700

    Fixed exception

--- Game.java
+++ Game.java
@@ -1,5 +1,6 @@
 package gameoflife;
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class Game {
@@ -7,28 +8,40 @@ class Game {
     static void start() {
         Scanner game = new Scanner(System.in);
         System.out.println("Количество по вертикали? (От 30 до 100)");
-        int y = game.nextInt();
-        if (y > 100) {
-            System.out.println("Количество клеток должно быть не больше 100!");
-            game.close();
-            return;
-        }
-        if (y < 30) {
-            System.out.println("Количество клеток должно быть не меньше 30!");
-            game.close();
+        int y;
+        try {
+            y = game.nextInt();
+            if (y > 100) {
+                System.out.println("Количество клеток должно быть не больше 100!");
+                game.close();
+                return;
+            }
+            if (y < 30) {
+                System.out.println("Количество клеток должно быть не меньше 30!");
+                game.close();
+                return;
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("Вы ввели не число!");
             return;
         }
 
         System.out.println("Количество клеток по горизонтали? (От 30 до 100)");
-        int x = game.nextInt();
-        if (x > 100) {
-            System.out.println("Количество клеток должно быть не больше 100!");
-            game.close();
-            return;
-        }
-        if (x < 30) {
-            System.out.println("Количество клеток должно быть не меньше 30!");
-            game.close();
+        int x;
+        try {
+            x = game.nextInt();
+            if (x > 100) {
+                System.out.println("Количество клеток должно быть не больше 100!");
+                game.close();
+                return;
+            }
+            if (x < 30) {
+                System.out.println("Количество клеток должно быть не меньше 30!");
+                game.close();
+                return;
+            }
+        } catch (NoSuchElementException e) {
+            System.out.println("Вы ввели не число!");
             return;
         }
 
